da41b78454801a46fa1314c41b06a0f653062e80
sbrycbc <sabriyecbc@gmail.com>
2024-01-17 16:12:43 +0100

    Examples were provided for continued learning...

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-var message = 'Hello, hello  World!!!!';
-var h1 = document.createElement('h1');
-h1.textContent = message;
-document.body.appendChild(h1);
+/* let message: string = 'Hello, hello  World!!!!';
+
+
+const h1=document.createElement('h1')
+h1.textContent=message
+document.body.appendChild(h1) */
+/* function getProduct(id) {
+    return {
+        id: id,
+        name: 'product #' + id,
+        price: 55,
+    };
+}
+function showProduct(name, price) {
+    return "product = ".concat(name, " und price = ").concat(price, "!");
+}
+var product = getProduct(1);
+console.log(product);
+product;
+ */
--- app.ts
+++ app.ts
@@ -1,6 +1,39 @@
-let message: string = 'Hello, hello  World!!!!';
+/* let message: string = 'Hello, hello  World!!!!';
 
 
 const h1=document.createElement('h1')
 h1.textContent=message
-document.body.appendChild(h1)
+document.body.appendChild(h1) */
+
+/* const reservedText = (text:string) => {
+    
+    return text.split('').reverse().join('')}
+
+    console.log(
+        reservedText(15)) */
+
+
+interface Product {
+    id:number,
+    name:string,
+    price:number
+}
+
+function getProduct(id):Product {
+    return {
+        id,
+        name:'product #' + id,
+        price:55,
+       
+    }
+} 
+
+function showProduct(name, price) {
+    return `product = ${name} und price = ${price}!`
+}
+
+const product = getProduct(1)
+
+console.log(product);
+
+product
